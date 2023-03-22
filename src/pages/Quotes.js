f531109f2c3d3be6958f5b476d799ac88f85747e
Michalous <49964028+Michalous@users.noreply.github.com>
2023-03-22 20:19:55 +0000

    another block removed

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -27,19 +27,3 @@ export default class InspirationalQuotes extends React.Component {
     )
   }
 }
-
-{/*
-import React from "react";
-
-function InspirationalQuotes() {
-    return (
-        <div>
-            <h1>Inspirational quotes:</h1>
-            <p>“The elevator to success is out of order. You’ll have to use the stairs, one step at a time.” Joe Girard</p>
-        </div>
-    )
-}
-*/}
-
-
-// export default InspirationalQuotes;
