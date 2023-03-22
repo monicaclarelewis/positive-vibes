94d2510b5e1db9e4edfd0136c9d6af588a216613
monicaclarelewis <118432326+monicaclarelewis@users.noreply.github.com>
2023-03-22 17:53:04 +0000

    navigation between pages implemented

---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div>
+        <h1> Happy Place! </h1> 
+        {/* Why doesn't this work? */}
+    </div>
+  )
+}
+
+export default Main
