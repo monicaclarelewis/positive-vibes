97bdd6086287cc6f15a7064e0d1ca4cefb32c2c0
Michalous <49964028+Michalous@users.noreply.github.com>
2023-03-22 19:11:41 +0000

    quotes component added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,7 @@ function Navbar() {
         <Link to="/"> Home </Link>
         <Link to="/Main"> Make me happy! </Link>
         <Link to="/jokes">Chuck Norris Banter</Link>
+        <Link to="/quotes">Inspirational Quotes</Link>
     </div>
   )
 }
