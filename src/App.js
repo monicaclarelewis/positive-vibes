3ef6f7af2058f5c4223685acbae2e53b58db0920
Piers Ebdon <37227905+piers12@users.noreply.github.com>
2023-03-22 18:54:58 +0000

    holding page for handling chuck norris jokes api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Main from "./pages/Main";
 import Navbar from "./components/Navbar";
+import Jokes from './pages/Jokes';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/main" element={<Main />} />
+            <Route path="/jokes" element={<Jokes />}/>
           </Routes>
       </Router>
     </div>
