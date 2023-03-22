df6c2affdd108b5da4eefc4170e3e02c1b2c69bc
monicaclarelewis <118432326+monicaclarelewis@users.noreply.github.com>
2023-03-22 20:01:33 +0000

    Tailwind downloads

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import "../styles/Home.css"
  function Home() {
    return (
      <div>
-        <h1>Happy ApP!</h1>
+        <h1>Happy App!</h1>
         {/* Why doesn't this work? */}
      </div>
    )
