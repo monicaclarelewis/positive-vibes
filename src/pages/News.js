42c122843a97990d52d413d6c2d39f5be35ea871
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-03-23 19:05:24 +0000

    added content to news.js

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from "axios";
 
-const News = () => {
-  return (
-    <div>Positive-news</div>
-  )
+
+const options = {
+  method: 'GET',
+  url: 'https://reuters-business-and-financial-news.p.rapidapi.com/article-date/01-04-2021',
+  headers: {
+    'X-RapidAPI-Key': '11f0fa9f76msh717eec8227350d2p15b301jsn2a98089fb824',
+    'X-RapidAPI-Host': 'reuters-business-and-financial-news.p.rapidapi.com'
+  }
+};
+
+class News extends Component {
+  state = {
+      News: ""
+  };
+  componentDidMount() {
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
+}    
 }
+
 
 export default News
