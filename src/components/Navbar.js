ce4047a11b9c09f54f048e0aca18a8c87b9490df
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-03-23 18:26:59 +0000

    added news.js to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,7 @@ function Navbar() {
         <Link to="/Main"> Make me happy! </Link>
         <Link to="/jokes">Chuck Norris Banter</Link>
         <Link to="/quotes">Inspirational Quotes</Link>
+        <Link to="/News">Positive-News</Link>
     </div>
   )
 }
