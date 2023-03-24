caeff1b823c3bccc8dcb588b300a3c9ac72f451d
Piers Ebdon <piers@peapps.co.uk>
2023-03-24 19:40:52 +0000

    add jokes to Main

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import "../styles/Main.css"
+import Jokes from './Jokes'
 
 function Main() {
   return (
     <div className="happyPlace">
         <div class="flex flex-col md:flex-row">
-          <div>ADD API HERE</div>
+          <div>{<Jokes />}</div>
 
           <div>ADD API HERE</div>
           
