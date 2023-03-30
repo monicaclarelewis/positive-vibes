7ba4f2d569cbd965f01644909317dc2d6d6673c8
monicaclarelewis <118432326+monicaclarelewis@users.noreply.github.com>
2023-03-30 14:38:18 +0100

    smile icon takes you to main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -11,21 +11,16 @@ import Party from '../components/Confetti/Confetti';
 function Main() {
   return (
     <div className="happyPlace">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <Card className="bg-amber-200 m-3 p-2">{<Jokes />}</Card>
-        <Card className="bg-amber-200 m-3 p-2">{<InspirationalQuotes />}</Card>
-        <Card className="bg-amber-200 m-3 p-2">{<News />}</Card>
-      </div>
-      <a href="/main">
-        <button className="bg-amber-400 hover:bg-amber-500 font-bold py-2 px-4 rounded-full border border border-black cursor-grab absolute inset-x-0 bottom-0 w-48 moreBtn">
-          MORE HAPPINESS!
-        </button>
-      </a>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <Card className="bg-amber-200 m-3 p-2">{<Jokes />}</Card>
+          <Card className="bg-amber-200 m-3 p-2">{<InspirationalQuotes />}</Card>
+          <Card className="bg-amber-200 m-3 p-2">{<News />}</Card>
+        </div>
       <div>
       <Party />
       </div>
     </div>
-  )
+  );
 }
 
 export default Main
